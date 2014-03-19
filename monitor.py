dfccd41d8a7c0d2fb6cc9d82033a4254f1680402
Justin Ely <ely@stsci.edu>
2014-03-19 14:08:23 -0400

    added steve to stim monitor mailer

--- monitor.py
+++ monitor.py
@@ -424,7 +424,7 @@ def send_email(missing_obs, missing_dates):
 
     svr_addr = 'smtp.stsci.edu'
     from_addr = 'ely@stsci.edu'
-    recipients = ['ely@stsci.edu', 'sahnow@stsci.edu']
+    recipients = ['ely@stsci.edu', 'sahnow@stsci.edu', 'penton@stsci.edu']
     to_addr = ', '.join(recipients)
 
     msg = MIMEMultipart()
