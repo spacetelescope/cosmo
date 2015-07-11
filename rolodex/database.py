8deaab25e5251251279b1e3afee349a8a4ff9959
Justin Ely <justincely@gmail.com>
2015-07-11 11:55:17 -0500

    parallel version for header and data table population

--- rolodex/database.py
+++ rolodex/database.py
@@ -101,7 +101,7 @@ def populate_lampflash():
 
 #-------------------------------------------------------------------------------
 
-def populate_primary_headers():
+def populate_primary_headers(num_cpu=1):
     print("adding to primary headers")
     files_to_add = [(result.id, os.path.join(result.path, result.name))
                         for result in session.query(Files).\
@@ -110,50 +110,56 @@ def populate_primary_headers():
                                 outerjoin(Headers, Files.id == Headers.file_id).\
                                 filter(Headers.file_id == None)]
 
+    args = [(full_filename, f_key) for f_key, full_filename in files_to_add]
 
-    ### Multiprocessing for this
-    for f_key, full_filename in files_to_add:
-        print(full_filename)
-
-        with fits.open(full_filename) as hdu:
-            session.add(Headers(filetype=hdu[0].header['filetype'],
-                               instrume=hdu[0].header['instrume'],
-                               rootname=hdu[0].header['rootname'],
-                               imagetyp=hdu[0].header['imagetyp'],
-                               targname=hdu[0].header['targname'],
-                               ra_targ=hdu[0].header['ra_targ'],
-                               dec_targ=hdu[0].header['dec_targ'],
-                               proposid=hdu[0].header['proposid'],
-                               qualcom1=hdu[0].header['qualcom1'],
-                               qualcom2=hdu[0].header['qualcom2'],
-                               qualcom3=hdu[0].header['qualcom3'],
-                               quality=hdu[0].header['quality'],
-                               cal_ver=hdu[0].header['cal_ver'],
-                               opus_ver=hdu[0].header['opus_ver'],
-                               obstype=hdu[0].header['obstype'],
-                               obsmode=hdu[0].header['obsmode'],
-                               exptype=hdu[0].header['exptype'],
-                               detector=hdu[0].header['detector'],
-                               segment=hdu[0].header['segment'],
-                               detecthv=hdu[0].header['detecthv'],
-                               life_adj=hdu[0].header['life_adj'],
-                               fppos=hdu[0].header['fppos'],
-                               exp_num=hdu[0].header['exp_num'],
-                               cenwave=hdu[0].header['cenwave'],
-                               aperture=hdu[0].header['aperture'],
-                               opt_elem=hdu[0].header['opt_elem'],
-                               shutter=hdu[0].header['shutter'],
-                               extended=hdu[0].header['extended'],
-                               obset_id=hdu[0].header['obset_id'],
-                               asn_id=hdu[0].header['asn_id'],
-                               asn_tab=hdu[0].header['asn_tab'],
-                               file_id=f_key))
-            session.commit()
+    pool = mp.Pool(processes=num_cpu)
+    pool.map(update_header, args)
 
 #-------------------------------------------------------------------------------
 
+def update_header((args)):
+    """update DB header table in parallel"""
 
-def populate_data():
+    filename, f_key = args
+    print(filename)
+    with fits.open(filename) as hdu:
+        session.add(Headers(filetype=hdu[0].header['filetype'],
+                           instrume=hdu[0].header['instrume'],
+                           rootname=hdu[0].header['rootname'],
+                           imagetyp=hdu[0].header['imagetyp'],
+                           targname=hdu[0].header['targname'],
+                           ra_targ=hdu[0].header['ra_targ'],
+                           dec_targ=hdu[0].header['dec_targ'],
+                           proposid=hdu[0].header['proposid'],
+                           qualcom1=hdu[0].header['qualcom1'],
+                           qualcom2=hdu[0].header['qualcom2'],
+                           qualcom3=hdu[0].header['qualcom3'],
+                           quality=hdu[0].header['quality'],
+                           cal_ver=hdu[0].header['cal_ver'],
+                           opus_ver=hdu[0].header['opus_ver'],
+                           obstype=hdu[0].header['obstype'],
+                           obsmode=hdu[0].header['obsmode'],
+                           exptype=hdu[0].header['exptype'],
+                           detector=hdu[0].header['detector'],
+                           segment=hdu[0].header['segment'],
+                           detecthv=hdu[0].header['detecthv'],
+                           life_adj=hdu[0].header['life_adj'],
+                           fppos=hdu[0].header['fppos'],
+                           exp_num=hdu[0].header['exp_num'],
+                           cenwave=hdu[0].header['cenwave'],
+                           aperture=hdu[0].header['aperture'],
+                           opt_elem=hdu[0].header['opt_elem'],
+                           shutter=hdu[0].header['shutter'],
+                           extended=hdu[0].header['extended'],
+                           obset_id=hdu[0].header['obset_id'],
+                           asn_id=hdu[0].header['asn_id'],
+                           asn_tab=hdu[0].header['asn_tab'],
+                           file_id=f_key))
+        session.commit()
+
+#-------------------------------------------------------------------------------
+
+def populate_data(num_cpu=1):
     print("adding to data")
     files_to_add = [(result.id, os.path.join(result.path, result.name))
                         for result in session.query(Files).\
@@ -161,55 +167,62 @@ def populate_data():
                                 outerjoin(Data, Files.id == Data.file_id).\
                                 filter(Data.file_id == None)]
 
+    args = [(full_filename, f_key) for f_key, full_filename in files_to_add]
 
-    ### Multiprocessing for this
-    for f_key, full_filename in files_to_add:
-        print(full_filename)
+    pool = mp.Pool(processes=num_cpu)
+    pool.map(update_data, args)
 
-        with fits.open(full_filename) as hdu:
+#-------------------------------------------------------------------------------
 
-            if len(hdu[1].data):
-                flux_mean=round(hdu[1].data['flux'].ravel().mean(), 5)
-                flux_max=round(hdu[1].data['flux'].ravel().max(), 5)
-                flux_std=round(hdu[1].data['flux'].ravel().std(), 5)
-            else:
-                flux_mean = None
-                flux_max = None
-                flux_std = None
+def update_data((args)):
+    """Update DB data table in parallel"""
 
-            session.add(Data(hvlevela=hdu[1].header.get('hvlevela', -999),
-                             hvlevelb=hdu[1].header.get('hvlevelb', -999),
-                             date_obs=hdu[1].header['date-obs'],
-                             time_obs=hdu[1].header['time-obs'],
-                             expstart=hdu[1].header['expstart'],
-                             expend=hdu[1].header['expend'],
-                             exptime=hdu[1].header['exptime'],
-                             shift1a=hdu[1].header.get('shift1a', -999),
-                             shift1b=hdu[1].header.get('shift1b', -999),
-                             shift1c=hdu[1].header.get('shift1c', -999),
-                             shift2a=hdu[1].header.get('shift2a', -999),
-                             shift2b=hdu[1].header.get('shift2b', -999),
-                             shift2c=hdu[1].header.get('shift2c', -999),
-                             sp_loc_a=hdu[1].header.get('sp_loc_a', -999),
-                             sp_loc_b=hdu[1].header.get('sp_loc_b', -999),
-                             sp_loc_c=hdu[1].header.get('sp_loc_c', -999),
-                             sp_nom_a=hdu[1].header.get('sp_nom_a', -999),
-                             sp_nom_b=hdu[1].header.get('sp_nom_b', -999),
-                             sp_nom_c=hdu[1].header.get('sp_nom_c', -999),
-                             sp_off_a=hdu[1].header.get('sp_off_a', -999),
-                             sp_off_b=hdu[1].header.get('sp_off_b', -999),
-                             sp_off_c=hdu[1].header.get('sp_off_c', -999),
-                             sp_err_a=hdu[1].header.get('sp_err_a', -999),
-                             sp_err_b=hdu[1].header.get('sp_err_b', -999),
-                             sp_err_c=hdu[1].header.get('sp_err_c', -999),
+    filename, f_key = args
+    print(filename)
 
-                             flux_mean=flux_mean,
-                             flux_max=flux_max,
-                             flux_std=flux_std,
-                             file_id=f_key))
+    with fits.open(filename) as hdu:
+        if len(hdu[1].data):
+            flux_mean=round(hdu[1].data['flux'].ravel().mean(), 5)
+            flux_max=round(hdu[1].data['flux'].ravel().max(), 5)
+            flux_std=round(hdu[1].data['flux'].ravel().std(), 5)
+        else:
+            flux_mean = None
+            flux_max = None
+            flux_std = None
+
+        session.add(Data(hvlevela=hdu[1].header.get('hvlevela', -999),
+                         hvlevelb=hdu[1].header.get('hvlevelb', -999),
+                         date_obs=hdu[1].header['date-obs'],
+                         time_obs=hdu[1].header['time-obs'],
+                         expstart=hdu[1].header['expstart'],
+                         expend=hdu[1].header['expend'],
+                         exptime=hdu[1].header['exptime'],
+                         shift1a=hdu[1].header.get('shift1a', -999),
+                         shift1b=hdu[1].header.get('shift1b', -999),
+                         shift1c=hdu[1].header.get('shift1c', -999),
+                         shift2a=hdu[1].header.get('shift2a', -999),
+                         shift2b=hdu[1].header.get('shift2b', -999),
+                         shift2c=hdu[1].header.get('shift2c', -999),
+                         sp_loc_a=hdu[1].header.get('sp_loc_a', -999),
+                         sp_loc_b=hdu[1].header.get('sp_loc_b', -999),
+                         sp_loc_c=hdu[1].header.get('sp_loc_c', -999),
+                         sp_nom_a=hdu[1].header.get('sp_nom_a', -999),
+                         sp_nom_b=hdu[1].header.get('sp_nom_b', -999),
+                         sp_nom_c=hdu[1].header.get('sp_nom_c', -999),
+                         sp_off_a=hdu[1].header.get('sp_off_a', -999),
+                         sp_off_b=hdu[1].header.get('sp_off_b', -999),
+                         sp_off_c=hdu[1].header.get('sp_off_c', -999),
+                         sp_err_a=hdu[1].header.get('sp_err_a', -999),
+                         sp_err_b=hdu[1].header.get('sp_err_b', -999),
+                         sp_err_c=hdu[1].header.get('sp_err_c', -999),
+
+                         flux_mean=flux_mean,
+                         flux_max=flux_max,
+                         flux_std=flux_std,
+                         file_id=f_key))
 
 
-            session.commit()
+        session.commit()
 
 #-------------------------------------------------------------------------------
 
@@ -220,7 +233,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     print(settings)
-    #insert_files(**settings)
+    insert_files(**settings)
     #populate_lampflash()
-    #populate_primary_headers()
-    populate_data()
+    #populate_primary_headers(settings['num_cpu'])
+    #populate_data(settings['num_cpu'])
