1c31bb8cbaa70fd2f5745d462906d1878a536a0d
Mees Fix <mfix@whitedwarf.stsci.edu>
2015-12-01 16:09:01 -0500

    wrote function to make CSV table.

--- scripts/create_master_csv.py
+++ scripts/create_master_csv.py
@@ -1,6 +1,8 @@
 import os
 import yaml
-from cos_monitoring.db_tables import load_connection
+from cos_monitoring.database.db_tables import load_connection
+from astropy.io import ascii
+from astropy.table import Table
 
 def main():
 
@@ -13,17 +15,20 @@ def main():
 
     #-- setup a connection to the databse
     Session, engine = load_connection(SETTINGS['connection_string'])
-    results = engine.execute("SELECT * FROM headers;")
-    for item in results:
-        print(item)
+    results = engine.execute("SELECT * FROM headers ORDER BY expstart;")
+    keys = results.keys()
 
 
-    #-- your code goes here
+    #-- genrate astropy table and output csv file.
+    def csv_generator(headers, keywords):
+        datarows = []
+        for item in headers:
+            datarows.append(item)
+        t = Table(rows = datarows, names = keywords, meta = {'Name':'COS HEADER TABLE'})
+        t.show_in_browser(jsviewer=True)
+        ascii.write(t, 'all_header.csv', format='csv')
 
-
-
-
-
+    csv_generator(results,keys)
 
     #-- close connections
     engine.dispose()
