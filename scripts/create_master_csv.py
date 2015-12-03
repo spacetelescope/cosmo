d22cbdbd151e911251250d36e2d93e1810bfa4c5
Mees Fix <mfix@whitedwarf.stsci.edu>
2015-12-03 15:16:00 -0500

    Added filename parameter and docstring for csv_generator

--- scripts/create_master_csv.py
+++ scripts/create_master_csv.py
@@ -4,6 +4,42 @@ from cos_monitoring.database.db_tables import load_connection
 from astropy.io import ascii
 from astropy.table import Table
 
+# add optional filename output to csv_generator
+# add documentation to the csv_generator function
+
+#-- genrate astropy table and output csv file.
+def csv_generator(headers, keywords, filename):
+    """
+    Pulls headers and keywords from SQL query to make a csv table.
+
+    Parameters
+    ---------------
+    headers: Class (<class 'sqlalchemy.engine.result.RowProxy'>)
+            All of the header information returned from SQL query.
+    keywords: List
+            A list of column names returned from SQL query.
+    filename: str
+            The names of CSV output file.
+
+    Returns
+    ---------------
+    None
+
+    Outputs
+    ---------------
+    t: CSV file
+            A CSV files that contains all of the queried header information.
+
+    """
+    datarows = []
+    for item in headers:
+        datarows.append(item)
+    print(type(item))
+    t = Table(rows = datarows, names = keywords, meta = {'Name':'COS HEADER TABLE'})
+    ascii.write(t, filename + '.csv', format='csv')
+
+
+
 def main():
 
     print("Querying the whole headers table...enjoy")
@@ -17,18 +53,8 @@ def main():
     Session, engine = load_connection(SETTINGS['connection_string'])
     results = engine.execute("SELECT * FROM headers ORDER BY expstart;")
     keys = results.keys()
-
-
-    #-- genrate astropy table and output csv file.
-    def csv_generator(headers, keywords):
-        datarows = []
-        for item in headers:
-            datarows.append(item)
-        t = Table(rows = datarows, names = keywords, meta = {'Name':'COS HEADER TABLE'})
-        t.show_in_browser(jsviewer=True)
-        ascii.write(t, 'all_header.csv', format='csv')
-
-    csv_generator(results,keys)
+    file_name = 'all_file'
+    csv_generator(results,keys,file_name)
 
     #-- close connections
     engine.dispose()
