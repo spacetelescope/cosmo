e31b642d97049c5d3d7316882287872eb11c584e
Jo Taylor <jotaylor@stsci.edu>
2016-04-11 16:00:12 -0400

    Updated init and setup to include retrieval scripts

--- cos_monitoring/__init__.py
+++ cos_monitoring/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 
 from .database import *
 from .filesystem import *
+from .retrieval import *
---
+++ cos_monitoring/retrieval/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from .find_new_cos_data import *
