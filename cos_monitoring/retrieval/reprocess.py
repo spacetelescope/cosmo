9ded7805511a651906ea546c5e99aa2717055d1f
Jo Taylor <jotaylor@stsci.edu>
2016-08-05 16:54:53 -0400

    Completed PID query function

--- cos_monitoring/retrieval/reprocess.py
+++ cos_monitoring/retrieval/reprocess.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python
 from __future__ import print_function, absolute_import, division
 '''
-Specify data to reprocess.
+Re-request and process COS data to keep products up to date.
 '''
 
 import argparse
 import yaml
 import os
+import pdb
 
 from .find_new_cos_data import janky_connect
 
@@ -14,7 +15,17 @@ from .find_new_cos_data import janky_connect
 #-----------------------------------------------------------------------------#
 def query_proposal(proposal):
     '''
-    Find all datasets of a given proposal ID.
+    Find all member datasets of a given proposal ID.
+
+    Parameters:
+    -----------
+        proposal : int
+            The proposal ID for which you wish to retrieve all data.
+    
+    Returns:
+    --------
+        prop_datasets : list
+            List of all datasets given a proposal ID. 
     '''
 
     # Open the configuration file for the MAST database connection (TSQL).
@@ -22,10 +33,27 @@ def query_proposal(proposal):
     with open(config_file, 'r') as f:
         SETTINGS = yaml.load(f)
 
-    query0 = "SELECT DISTINCT ads_program_id FROM "\
-    "archive_data_set_all WHERE ads_pep_id='{0}'".format(str(proposal))
+    # Need to use opus_rep DB instead of dadsops_rep to get ALL datasets
+    SETTINGS["database"] = "opus_rep"
+    query0 = "SELECT DISTINCT program_id FROM executed "\
+    "WHERE proposal_id='{0}'\ngo".format(str(proposal))
+    vid = janky_connect(SETTINGS, query0)
 
-    prop
+    # Now we need to go back to dadsop_rep to get ads_data_set_name
+    SETTINGS["database"] = "dadsops_rep"
+    query1 = "SELECT DISTINCT ads_data_set_name,ads_pep_id FROM archive_data_set_all "\
+    "WHERE ads_data_set_name LIKE '_{0}%'\ngo".format(str(vid[0]))
+    prop_datasets = janky_connect(SETTINGS, query1)
+
+    return prop_datasets
+
+#-----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+def handle_datasets(datasets):
+    '''
+    Get proposal IDs and, if applicable, all member datasets given a visit
+    '''
+
 
 
 #-----------------------------------------------------------------------------#
@@ -47,8 +75,7 @@ if __name__ == "__main__":
             prop_datasets = query_proposal(prop)
         except ValueError:
             isproposal.append(False)
-            handle_dataset(item)        
+            handle_datasets(item)        
     
-
     print(args.data)
 
