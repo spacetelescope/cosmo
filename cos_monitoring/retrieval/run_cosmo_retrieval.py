896ed67a8695d8e7801fea98d535e877efb3ed15
Jo Taylor <jotaylor@stsci.edu>
2017-03-29 16:32:42 -0400

    Fixed 2 typos

--- cos_monitoring/retrieval/run_cosmo_retrieval.py
+++ cos_monitoring/retrieval/run_cosmo_retrieval.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
 from .retrieval_info import BASE_DIR, CACHE
-from .manualabor import work_laboriousy, chmod, PERM_755, PERM_872
+from .manualabor import work_laboriously, chmod, PERM_755, PERM_872
 from .find_new_cos_data import find_new_cos_data
 from .request_data import run_all_retrievals
 
 # Change permissions on base directory.
-chmod(basepath=BASE_DIR, mode=PERM_755, filetype=None, recursive=True
+chmod(basepath=BASE_DIR, mode=PERM_755, filetype=None, recursive=True)
 
 # Run find_new_cos_data
 find_new_cos_data(pkl_it=True, use_cs=True, run_labor=True, do_chmod=False)
