44365c70a76c57ef1850c5625453d4423f81975a
Jo Taylor <jotaylor@stsci.edu>
2018-05-15 16:16:47 -0400

    Separated out work_laboriously (renamed to calibrate_data) and make_csum()

---
+++ cos_monitoring/retrieval/calibrate_data.py
@@ -0,0 +1,156 @@
+#! /usr/bin/env python
+
+from __future__ import print_function, absolute_import, division
+
+'''
+This ia a program designed to calibrate COS rawfiles to create CSUMs.
+
+It also sets permissions and group ids appropriately as well
+as zipping up any unzipped files to save space.
+'''
+
+__author__ = "Jo Taylor"
+__date__ = "04-13-2016"
+__maintainer__ = "Jo Taylor"
+
+import datetime
+import os
+import glob
+import calcos
+import argparse
+import stat
+
+from .dec_calcos import clobber_calcos_csumgz
+from .hack_chmod import chmod
+from .retrieval_info import BASE_DIR, CACHE
+from .manualabor import (handle_nullfiles, gzip_files, get_unprocessed_data, 
+    parallelize, copy_outdirs, remove_outdirs) 
+
+PERM_755 = stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
+PERM_872 = stat.S_ISVTX | stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
+CSUM_DIR = "tmp_out"
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+def make_csum(unzipped_raws):
+    '''
+    Calibrate raw files to produce csum files.
+
+    Parameters:
+    -----------
+        unzipped_raws : list or string
+            A list or string filenames that are unzipped to be calibrated.
+
+    Returns:
+    --------
+        Nothing
+    '''
+
+    run_calcos = clobber_calcos_csumgz(calcos.calcos)
+    if isinstance(unzipped_raws, str):
+        unzipped_raws = [unzipped_raws]
+    for item in unzipped_raws:
+        dirname = os.path.dirname(item)
+        outdirec = os.path.join(dirname, CSUM_DIR)
+        if not os.path.exists(outdirec):
+            try:
+                os.mkdir(outdirec)
+            except FileExistsError:
+                pass
+        try:
+            run_calcos(item, outdir=outdirec, verbosity=0,
+                       create_csum_image=True, only_csum=True,
+                       compress_csum=False)
+        except Exception as e:
+            if type(e).__name__ == "IOError" and \
+               e.args[0] == "Empty or corrupt FITS file":
+                print("="*72 + "\n" + "="*72)
+                print("The file is empty or corrupt: {0}".format(item))
+                print("Deleting file")
+                print("="*72 + "\n" + "="*72)
+                os.remove(item)
+                pass
+            else:
+                print(e)
+                pass
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+@timefunc
+def calibrate_data(prl=True, do_chmod=False):
+    '''
+    Run all the functions in the correct order.
+
+    Parameters:
+    -----------
+        prl : Boolean
+            Switch for running functions in parallel
+
+    Returns:
+    --------
+        Nothing
+    '''
+
+    remove_outdirs()
+
+    # First, change permissions of the base directory so we can modify files.
+    print("Beginning calibration procedures at {}...\n".format(datetime.datetime.now()))
+    if do_chmod: 
+        print("Opening permissions of {0}..".format(BASE_DIR))
+        chmod(BASE_DIR, PERM_755, None, True)
+
+    # Delete any files with program ID = NULL that are not COS files.
+    nullfiles = glob.glob(os.path.join(BASE_DIR, "NULL", "*fits*")) 
+    if nullfiles:
+        print("Handling {0} NULL datasets...".format(len(nullfiles)))
+        handle_nullfiles(nullfiles) 
+    # Zip any unzipped files, if they exist.
+    gzip_files(prl)
+
+    # Get list of files that need to be processed.
+    to_calibrate = get_unprocessed_data()
+
+    # If there are files to calibrate, create csums for them.
+    if to_calibrate:
+        print("There are {0} file(s) to calibrate, beginning now.".format(len(to_calibrate)))
+        if prl:
+            parallelize(make_csum, to_calibrate)
+        else:
+            make_csum(to_calibrate)
+        num_files += max_files
+    
+    # Output csums are put in temporary output directories to avoid overwriting
+    # intermediate products. Copy only the csums from these directories into 
+    # their parent PID directories.
+    copy_outdirs()
+
+    # To be safe, check again for unzipped files, and zip them.
+    gzip_files(prl)
+        
+    # Check for the temporary output directories used during calibration,
+    # and delete if present. 
+    remove_outdirs()
+
+    # Change permissions back to protect data.
+    if do_chmod:
+        print("Closing permissions of {0}..".format(BASE_DIR))
+        chmod(BASE_DIR, PERM_872, None, True)
+        print("Changing group permissions of {0}...".format(BASE_DIR))
+        chgrp(BASE_DIR)
+
+    print("\nFinished at {0}.".format(datetime.datetime.now()))
+
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prl", dest="prl", action="store_true",
+                        default=False, help="Parallellize functions")
+    parser.add_argument("--chmod", dest="do_chmod", action="store_false",
+                        default=True, help="Switch to turn off chmod")
+    args = parser.parse_args()
+
+    calibrate_data(args.prl, args.do_chmod)
