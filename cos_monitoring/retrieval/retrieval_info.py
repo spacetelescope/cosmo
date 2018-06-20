074e11f2e86f1b1eb699e424cacdc7eb05b31976
Jo Taylor <jotaylor@stsci.edu>
2018-06-20 13:29:19 -0400

    Added PERM_755, PERM_872, CSUM_DIR constants

--- cos_monitoring/retrieval/retrieval_info.py
+++ cos_monitoring/retrieval/retrieval_info.py
@@ -1,3 +1,7 @@
+import stat
+
 BASE_DIR = "/grp/hst/cos2/cosmo"
 CACHE = "/ifs/archive/ops/hst/public"
-TESTMODE = False
+PERM_755 = stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
+PERM_872 = stat.S_ISVTX | stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
+CSUM_DIR = "tmp_out"
