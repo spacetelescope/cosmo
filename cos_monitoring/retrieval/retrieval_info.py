332eaeaade16f5fa680d1ddb72b16bb0b481bd30
Camellia Magness <cmagness@stsci.edu>
2018-10-05 18:00:05 -0400

    Changing credentials to me (cmagness). Close #35

--- cos_monitoring/retrieval/retrieval_info.py
+++ cos_monitoring/retrieval/retrieval_info.py
@@ -6,4 +6,4 @@ PERM_755 = stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IX
 PERM_872 = stat.S_ISVTX | stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
 PERM_550 = stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP
 CSUM_DIR = "tmp_out"
-USERNAME = "jotaylor"
+USERNAME = "cmagness"
