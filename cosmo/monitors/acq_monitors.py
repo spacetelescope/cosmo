4469981a7c4602f82da0dfcd8f9d7ac2345c4f1a
James White <jwhite@stsci.edu>
2019-05-06 16:01:51 -0400

    Add draft plot notes and todo

--- cosmo/monitors/acq_monitors.py
+++ cosmo/monitors/acq_monitors.py
@@ -573,3 +573,35 @@ class AcqPeakxdMonitor(BaseMonitor):
         layout = go.Layout(updatemenus=updatemenus, hovermode='closest')
         self.figure.add_traces(traces)
         self.figure['layout'].update(layout)
+
+# TODO: Add OSM Shift monitor and datamodel. Draft output below after data is retrieved and put in a dataframe
+# traces = []
+# # figure = go.Figure()
+# # idx = 0
+# # for name, group in groups:
+# #     group = group.sort_values('EXPSTART')
+# #
+# #     t = []
+# #     lp = []
+# #     for i, row in group.iterrows():
+# #         row_t = [Time(row.EXPSTART, format='mjd').datetime + datetime.timedelta(seconds=lil_t) for lil_t in row.TIME]
+# #         row_lp = [f'LP{row.LIFE_ADJ}' for _ in range(len(row.TIME))]
+# #         lp += row_lp
+# #         t += row_t
+# #
+# #     traces.append(
+# #         go.Scattergl(
+# #             x=t,
+# #             y=[item for sublist in group.SHIFT_DISP.values for item in sublist],
+# #             name='-'.join([str(item) for item in name]),
+# #             mode='markers',
+# #             marker=dict(
+# #                 cmax=19,
+# #                 cmin=0,
+# #                 color=list(repeat(idx, len(t))),
+# #                 colorscale='Viridis'
+# #             ),
+# #             text=lp
+# #         )
+# #     )
+# #     idx += 1
