47588e6a13246cb1fba83f37a5b42d84961e367b
James White <jwhite@stsci.edu>
2019-07-10 20:06:29 -0400

    Refactor to plot per grating

--- cosmo/monitors/osm_drift_monitors.py
+++ cosmo/monitors/osm_drift_monitors.py
@@ -19,7 +19,7 @@ class OSMDriftMonitor(BaseMonitor):
 
     def track(self):
         """Track the drift for Shift1 and Shift2."""
-        # Calculate the relative shift for AD and XD
+        # Calculate the relative shift (relative to the first shift measurement for each set of flashes) for AD and XD
         self.filtered_data['REL_SHIFT_DISP'] = self.filtered_data.apply(
             lambda x: x.SHIFT_DISP - x.SHIFT_DISP[0] if len(x.SHIFT_DISP) else x.SHIFT_DISP, axis=1
         )
@@ -33,6 +33,7 @@ class OSMDriftMonitor(BaseMonitor):
             self.filtered_data, ['TIME', 'SHIFT_DISP', 'SHIFT_XDISP', 'SEGMENT', 'REL_SHIFT_DISP', 'REL_SHIFT_XDISP']
         )
 
+        # Add drift columns and time since OSM move columns
         exploded = exploded.assign(
             SHIFT1_DRIFT=lambda x: x.REL_SHIFT_DISP / x.TIME,
             SHIFT2_DRIFT=lambda x: x.REL_SHIFT_XDISP / x.TIME,
@@ -43,6 +44,7 @@ class OSMDriftMonitor(BaseMonitor):
         return exploded
 
     def filter_data(self):
+        """Filter data on detector."""
         return self.data[self.data.DETECTOR == self.detector].reset_index(drop=True)
 
     def store_results(self):
@@ -59,25 +61,26 @@ class FUVOSMDriftMonitor(OSMDriftMonitor):
         ynames = ['SHIFT1_DRIFT', 'SHIFT2_DRIFT']
         titles = ['OSM1 SHIFT1', 'OSM1 SHIFT2']
 
-        for y, name, axes in zip(ynames, titles, locations):
-            trace = go.Scattergl(
-                x=self.results.REL_TSINCEOSM1,
-                y=self.results[y],
-                mode='markers',
-                name=name,
-                text=self.results.hover_text,
-                marker=dict(
-                    color=self.results.EXPSTART,
-                    colorscale='Viridis',
-                    showscale=True,
-                    colorbar=dict(
-                        len=0.75,
-                        title='EXPSTART [mjd]'
+        for grating, group in self.results.groupby('OPT_ELEM'):
+            for y, name, axes in zip(ynames, titles, locations):
+                trace = go.Scattergl(
+                    x=group.REL_TSINCEOSM1,
+                    y=group[y],
+                    mode='markers',
+                    name=f'{grating} {name}',
+                    text=group.hover_text,
+                    marker=dict(
+                        color=group.EXPSTART,
+                        colorscale='Viridis',
+                        showscale=True,
+                        colorbar=dict(
+                            len=0.65,
+                            title='EXPSTART [mjd]'
+                        ),
                     ),
-                ),
-            )
+                )
 
-            self.figure.append_trace(trace, *axes)
+                self.figure.append_trace(trace, *axes)
 
         layout = go.Layout(
             title=f'{self.detector} {self.name}',
@@ -97,36 +100,41 @@ class NUVOSMDriftMonitor(OSMDriftMonitor):
     def plot(self):
         xnames = ['REL_TSINCEOSM1', 'REL_TSINCEOSM1', 'REL_TSINCEOSM2', 'REL_TSINCEOSM2']
         ynames = ['SHIFT1_DRIFT', 'SHIFT2_DRIFT', 'SHIFT1_DRIFT', 'SHIFT2_DRIFT']
+        titles = ['OSM1 SHIFT1', 'OSM1 SHIFT2', 'OSM2 SHIFT1', 'OSM2 SHIFT2']
         locations = [(1, 1), (2, 1), (1, 2), (2, 2)]
 
-        for x, y, axes in zip(xnames, ynames, locations):
-            trace = go.Scattergl(
-                x=self.results[x],
-                y=self.results[y],
-                mode='markers',
-                text=self.results.hover_text,
-                marker=dict(
-                    color=self.results.EXPSTART,
-                    colorscale='Viridis',
-                    showscale=True,
-                    colorbar=dict(
-                        len=0.75,
-                        title='EXPSTART [mjd]'
-                    )
-                ),
-            )
+        for grating, group in self.results.groupby('OPT_ELEM'):
+            for x, y, axes, name in zip(xnames, ynames, locations, titles):
+                trace = go.Scattergl(
+                    x=group[x],
+                    y=group[y],
+                    mode='markers',
+                    text=group.hover_text,
+                    name=f'{grating} {name}',
+                    marker=dict(
+                        color=group.EXPSTART,
+                        cmin=self.results.EXPSTART.min(),
+                        cmax=self.results.EXPSTART.max(),
+                        colorscale='Viridis',
+                        showscale=True,
+                        colorbar=dict(  # TODO: Move the colorbar location down
+                            len=0.45,
+                            title='EXPSTART [mjd]'
+                        )
+                    ),
+                )
 
-            self.figure.append_trace(trace, *axes)
+                self.figure.append_trace(trace, *axes)
 
         layout = go.Layout(
             title=f'{self.detector} {self.name}',
             xaxis=dict(title='Time since last OSM1 move [s]'),
-            xaxis2=dict(title='Time since last OSM1 move [s]'),
-            xaxis3=dict(title='Time since last OSM2 move [s]'),
+            xaxis3=dict(title='Time since last OSM1 move [s]'),
+            xaxis2=dict(title='Time since last OSM2 move [s]'),
             xaxis4=dict(title='Time since last OSM2 move [s]'),
             yaxis=dict(title='SHIFT1 drift rate [pixels/sec]'),
-            yaxis2=dict(title='SHIFT2 drift rate [pixels/sec]'),
-            yaxis3=dict(title='SHIFT1 drift rate [pixels/sec]'),
+            yaxis3=dict(title='SHIFT2 drift rate [pixels/sec]'),
+            yaxis2=dict(title='SHIFT1 drift rate [pixels/sec]'),
             yaxis4=dict(title='SHIFT2 drift rate [pixels/sec]')
         )
 
