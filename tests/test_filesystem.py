2ee64cddcca15c0bb0707bd607c4472bb96434b3
James White <jwhite@stsci.edu>
2019-07-18 17:07:30 -0400

    Check absolute path of file specified in environment variable

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -10,7 +10,7 @@ TEST_DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/')
 TEST_CONFIG = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cosmoconfig_test.yaml')
 
 # Check to make sure that the test config file is being used. If not, don't run the tests
-if os.environ['COSMO_CONFIG'] != TEST_CONFIG:
+if os.path.abspath(os.environ['COSMO_CONFIG']) != TEST_CONFIG:
     raise TypeError('Tests should only be executed with the testing configuration file')
 
 ARGS = (
