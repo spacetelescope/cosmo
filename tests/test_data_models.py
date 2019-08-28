705bc1b01473e8921f55e4433008658102cb3f88
James White <jwhite@stsci.edu>
2019-08-28 10:57:12 -0400

    Add ingest tests

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -33,6 +33,11 @@ class TestOSMDataModel:
 
         request.cls.osmmodel = osmmodel  # Add the data model to the test class
 
+        yield
+
+        if request.cls.osmmodel.model is not None:
+            request.cls.osmmodel.model.drop_table(safe=True)
+
     def test_data_collection(self):
         assert isinstance(self.osmmodel.new_data, pd.DataFrame)
         assert len(self.osmmodel.new_data) == 11  # There are 11 test data sets
@@ -61,6 +66,12 @@ class TestOSMDataModel:
         for key in data_extension_keys:
             assert isinstance(self.osmmodel.new_data[key].values, np.ndarray)
 
+    def test_data_ingest(self):
+        self.osmmodel.ingest()
+
+        assert self.osmmodel.model is not None
+        assert len(list(self.osmmodel.model.select())) == 11
+
 
 class TestAcqDataModel:
 
@@ -70,7 +81,16 @@ class TestAcqDataModel:
 
         request.cls.acqmodel = acqmodel
 
+        yield
+
+        if request.cls.acqmodel.model is not None:
+            request.cls.acqmodel.model.drop_table(safe=True)
+
     def test_data_collection(self):
+        assert isinstance(self.acqmodel.new_data, pd.DataFrame)
+        assert len(self.acqmodel.new_data) == 9  # There are 9 test data sets
+
+    def test_content_collected(self):
         keys_that_should_be_there = (
             # Header keywords
             'ACQSLEWX', 'ACQSLEWY', 'EXPSTART', 'ROOTNAME', 'PROPOSID', 'OBSTYPE', 'NEVENTS', 'SHUTTER', 'LAMPEVNT',
@@ -86,3 +106,9 @@ class TestAcqDataModel:
         # Check that the FGS column was created correctly
         assert 'FGS' in self.acqmodel.new_data
         assert sorted(self.acqmodel.new_data.FGS.unique()) == ['F1', 'F2', 'F3']
+
+    def test_data_ingest(self):
+        self.acqmodel.ingest()
+
+        assert self.acqmodel.model is not None
+        assert len(list(self.acqmodel.model.select())) == 9
