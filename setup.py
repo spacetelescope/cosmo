b134c1866100ac4b97fa20765683ac5c282e9928
James White <jwhite@stsci.edu>
2019-10-22 13:34:35 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cosmo',
-    version='1.0.1',
+    version='1.1.0',
     description='Monitors for HST/COS',
     keywords=['astronomy'],
     classifiers=[
