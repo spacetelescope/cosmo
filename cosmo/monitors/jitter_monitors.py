ddb9d22613670c38637b2bff698742b19f23015e
James White <jwhite@stsci.edu>
2020-01-06 13:59:55 -0500

    Add jitter monitor notes

---
+++ cosmo/monitors/jitter_monitors.py
@@ -0,0 +1,4 @@
+# TODO: Jitter Monitor needs to include 2 (possibly 3) subplots of statistics on the jitter per exposure.
+#  Plot 1: mean vs time with +/- std "line boundaries"
+#  Plot 2: max vs time
+#  Plot 3 (potentially): std vs time
