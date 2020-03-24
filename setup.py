41274be5dfe80caa7ec94fd162a727cd319ce1d7
James White <jwhite@stsci.edu>
2020-03-24 14:24:45 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cosmo',
-    version='1.1.0',
+    version='1.1.1',
     description='Monitors for HST/COS',
     keywords=['astronomy'],
     classifiers=[
