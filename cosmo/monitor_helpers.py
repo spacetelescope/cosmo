b6afa79f235292ffac1ace1d4071020992412a88
chjohnson1 <60275244+chjohnson1@users.noreply.github.com>
2020-06-17 17:37:39 -0400

    updated convert_day_of_year
    Fixed an issue where the str(date) command would truncate trailing zeros in the day of year.

--- cosmo/monitor_helpers.py
+++ cosmo/monitor_helpers.py
@@ -11,7 +11,7 @@ def convert_day_of_year(date: Union[float, str]) -> Time:
     """Convert day of the year (defined as yyyy.ddd where ddd is the day number of that year) to an astropy Time object.
     Some important dates for the COS team were recorded in this format.
     """
-    return Time(datetime.datetime.strptime(str(date), '%Y.%j'), format='datetime')
+    return Time(datetime.datetime.strptime('{:.3f}'.format(date), '%Y.%j'), format='datetime')
 
 
 def fit_line(x: Sequence, y: Sequence) -> Tuple[np.poly1d, np.ndarray]:
