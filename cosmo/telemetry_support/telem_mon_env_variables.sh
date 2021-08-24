89534a03021a6b7db0be56981ec0d9e72c5610e6
nkerman <nkerman@stsci.edu>
2021-08-24 12:55:40 -0400

    Add sh script of environment variables

---
+++ cosmo/telemetry_support/telem_mon_env_variables.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+# Where do the telemetry data files live? (unlikely to change this)
+export TELEMETRY_DATADIR="/grp/hst/cos/Telemetry/"
+
+# Where do you want the output plots to live? (likely to change this)
+export TELEMETRY_PLOTSDIR="/user/nkerman/Projects/Monitors/telemetry_plots/"
+
+# Where does the mnemonics excel file live? (likely to change this)
+export TELEMETRY_MNEMONICS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/COSMnemonics.xls"
+
+# Where does the zoom-specifying csv file live? (likely to change this)
+export TELEMETRY_ZOOMS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/default_telemetry_zooms.csv"
