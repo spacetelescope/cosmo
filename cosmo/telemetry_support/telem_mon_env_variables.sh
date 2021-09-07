32bcc1f0d4a1bb72744dfeeed42e972fa3808795
nkerman <nkerman@stsci.edu>
2021-09-07 14:46:48 -0400

    Added env var for the json file

--- cosmo/telemetry_support/telem_mon_env_variables.sh
+++ cosmo/telemetry_support/telem_mon_env_variables.sh
@@ -8,5 +8,8 @@ export TELEMETRY_PLOTSDIR="/user/nkerman/Projects/Monitors/telemetry_plots/"
 # Where does the mnemonics excel file live? (likely to change this)
 export TELEMETRY_MNEMONICS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/COSMnemonics.xls"
 
+#  Path to the (JSON) file where you specify the numerical value of each telemetry variable whose value is a text string (i.e. the OSM positions)
+export TELEMETRY_TEXTS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/telemetry_monitors_text.json"
+
 # Where does the zoom-specifying csv file live? (likely to change this)
 export TELEMETRY_ZOOMS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/default_telemetry_zooms.csv"
