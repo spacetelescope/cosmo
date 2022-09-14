2796daeb4ce68b6c6705b779ec7a038756298c3b
nkerman <nathaniel.kerman@gmail.com>
2022-09-14 14:54:01 -0400

    Improve compatibility of telem monitor env variables

--- cosmo/telemetry_support/telem_mon_env_variables.sh
+++ cosmo/telemetry_support/telem_mon_env_variables.sh
@@ -3,13 +3,14 @@
 export TELEMETRY_DATADIR="/grp/hst/cos/Telemetry/"
 
 # Where do you want the output plots to live? (likely to change this)
-export TELEMETRY_PLOTSDIR="/user/nkerman/Projects/Monitors/telemetry_plots/"
+export TELEMETRY_PLOTSDIR="/grp/hst/cos2/monitoring/telemetry/latest"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Where does the mnemonics excel file live? (likely to change this)
-export TELEMETRY_MNEMONICS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/COSMnemonics.xls"
+export TELEMETRY_MNEMONICS="$SCRIPT_DIR/COSMnemonics.xls"
 
 #  Path to the (JSON) file where you specify the numerical value of each telemetry variable whose value is a text string (i.e. the OSM positions)
-export TELEMETRY_TEXTS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/telemetry_monitors_text.json"
+export TELEMETRY_TEXTS="$SCRIPT_DIR/telemetry_monitors_text.json"
 
 # Where does the zoom-specifying csv file live? (likely to change this)
-export TELEMETRY_ZOOMS="/home/nkerman/Projects/Monitors/cosmo/cosmo/telemetry_support/default_telemetry_zooms.csv"
+export TELEMETRY_ZOOMS="$SCRIPT_DIR/default_telemetry_zooms.csv"
